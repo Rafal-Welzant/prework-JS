8ea48c312a08c8700b10be1d0387647b236924b5
Rafal Welzant <welzant@gmail.com>
2021-11-12 15:25:13 +0100

    logical operators

--- test.js
+++ test.js
@@ -1,14 +1,8 @@
-var myAge = 29;
+var myAge = 25;
 
-if (myAge > 30){
-    document.write("you are over 30!");
-} else if (myAge > 20){
-    document.write("you are over 20!");
-
-} else if (myAge > 10){
-    document.write("you are over 10!");
-
+if (myAge >= 18 && myAge <= 30){
+    document.write("you can come, you cool dude");
 } else {
-    document.write("you are not over 10!");
-    
+    document.write("you aint comin!");
 }
+    
